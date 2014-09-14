1a372a7f189816005f5773d4dadd7655837bf0c0
Mikko Vesikkala <mikko.vesikkala@iki.fi>
2014-09-14 13:40:37 +0300

    newsfeed

--- js/init.js
+++ js/init.js
@@ -3,8 +3,8 @@
   skel.init({
     reset: 'full',
     breakpoints: {
-      'global': { range: '*', href: 'css/style.css', containers: 1000, grid: { gutters: 50 } },
-      'normal': { range: '-1280', href: 'css/laptop.css', containers: 960, grid: { gutters: 30 } },
+      'global': { range: '*', href: 'css/style.css', containers: 950, grid: { gutters: 50 } },
+      'normal': { range: '-1280', href: 'css/laptop.css', containers: 900, grid: { gutters: 30 } },
       'narrow': { range: '-1000', href: 'css/tablet.css', containers: '100%', grid: { gutters: 25, collapse: true } },
       'mobile': { range: '-640', href: 'css/mobile.css', grid: { gutters: 10 }, viewport: { scalable: false } }
     }
@@ -30,7 +30,10 @@
         var postElement = $('<div class="fb-post">')
         postElement.append($('<span>').text(dateString))
         if (post.picture) {
+          postElement.addClass('image')
           postElement.append($('<a>').attr('href', post.link).append($('<img>').attr('src', post.picture)))
+        } else {
+          postElement.append($('<a>').attr('href', 'http://fb.com/audiopoli').append($('<i class="fa fa-facebook fa-2"></i>')))
         }
         postElement.append($('<p>').text(post.message))
         postElements.push(postElement)
